c2b8ccd130d226e073432c9bbdea8013840ecd50
Radu Iamandi <iamandiradustefan@gmail.com>
2024-08-08 00:50:29 +0300

    improve UI

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,17 +3,31 @@ import { RouterOutlet } from '@angular/router';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { SummaryComponent } from './summary/summary.component';
 
+export interface ExpandedDetails {
+  description: string;
+  start: Date;
+  end: Date;
+  duration: number;
+  overlap?: boolean;
+}
+export interface ProcessedData {
+  date: string;
+  hoursWorkedInt: number;
+  hoursWorked?: string;
+  flags: string;
+  entries: number;
+  expandedDetails: ExpandedDetails[];
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, FileUploadComponent, SummaryComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
 export class AppComponent {
   title = 'angusheet';
-}
 
   summaryData: ProcessedData[] = [];
 
